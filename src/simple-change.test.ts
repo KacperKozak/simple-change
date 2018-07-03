26a09226b163bd65f342b1bead90b402f24d6925
Kacper Kozak <kacper.kozak@siili.com>
2018-07-03 15:23:12 +0200

    Init

---
+++ src/simple-change.test.ts
@@ -0,0 +1,7 @@
+import { change } from './simple-change';
+
+describe('simple-change', () => {
+    it('should work', () => {
+        expect(change).toBeDefined();
+    });
+});
---
+++ src/simple-change.ts
@@ -0,0 +1,1 @@
+export const change = (any: any) => any;
